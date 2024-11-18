770f343c1651d4d1f6d389e80289983af005b90e
AlonsoErnesto <ernesto134alonso123@gmail.com>
2024-11-18 12:05:07 -0500

    feeat(session):session with mongo

--- database.js
+++ database.js
@@ -1,27 +1,40 @@
-//The require(‘mongoose’) call above returns a Singleton object.
-//It means that the first time you call require(‘mongoose’), it
-//is creating an instance of the Mongoose class and returning it.
-//On subsequent calls, it will return the same instance that was
-//created and returned to you the first time because of how module
-//import/export works in ES6.
 const mongoose = require("mongoose");
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 require('dotenv').config({ path: '.env' });
+const express = require('express');
 
 class Database {
 
     constructor() {
-        this.connect();
+        this.app = express(); // Crear instancia de Express
+        this.connect(); // Conectar a la base de datos
+        this.setupSession(); // Configurar las sesiones
     }
 
+    // Conectar a MongoDB
     connect() {
-        mongoose.connect(process.env.DATABASE_MONGODB)
+        mongoose.connect(process.env.DATABASE_MONGODB, )
             .then(() => {
-                console.log("database connection successful");
+                console.log("Database connection successful");
             })
             .catch((err) => {
-                console.log("database connection error " + err);
+                console.log("Database connection error: " + err);
             });
+    }
 
+    // Configurar las sesiones con MongoDB
+    setupSession() {
+        this.app.use(session({
+            secret: 'your-secret',  // Cambia esto por un secreto más seguro
+            resave: false,
+            saveUninitialized: false,
+            store: new MongoStore({
+                mongoUrl: process.env.DATABASE_MONGODB,  // Especificar la URL de la base de datos MongoDB
+                ttl: 14 * 24 * 60 * 60  // Tiempo de vida de la sesión en segundos (14 días)
+            }),
+            cookie: { secure: process.env.NODE_ENV === 'production' }  // Usa cookies seguras en producción
+        }));
     }
 }
 
