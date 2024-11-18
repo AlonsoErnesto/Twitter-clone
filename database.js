64ad302163374378c8be0f9db544a70f13120148
AlonsoErnesto <ernesto134alonso123@gmail.com>
2024-11-18 10:53:33 -0500

    feat(debuging):Image and DB

--- database.js
+++ database.js
@@ -1,14 +1,14 @@
-//The require(‘mongoose’) call above returns a Singleton object. 
-//It means that the first time you call require(‘mongoose’), it 
-//is creating an instance of the Mongoose class and returning it. 
-//On subsequent calls, it will return the same instance that was 
-//created and returned to you the first time because of how module 
+//The require(‘mongoose’) call above returns a Singleton object.
+//It means that the first time you call require(‘mongoose’), it
+//is creating an instance of the Mongoose class and returning it.
+//On subsequent calls, it will return the same instance that was
+//created and returned to you the first time because of how module
 //import/export works in ES6.
 const mongoose = require("mongoose");
+require('dotenv').config({ path: '.env' });
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
-mongoose.set('useUnifiedTopology', true);
 
 class Database {
 
@@ -17,13 +17,15 @@ class Database {
     }
 
     connect() {
-        mongoose.connect("URL:DATABASE-MONGODB")
-        .then(() => {
-            console.log("database connection successful");
-        })
-        .catch((err) => {
-            console.log("database connection error " + err);
-        })
+        console.log('DATABASE_MONGODB:', process.env.DATABASE_MONGODB); // Verifica si la URL de MongoDB está cargada
+        mongoose.connect(process.env.DATABASE_MONGODB)
+            .then(() => {
+                console.log("database connection successful");
+            })
+            .catch((err) => {
+                console.log("database connection error " + err);
+            });
+
     }
 }
 
